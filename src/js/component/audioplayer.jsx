7d736955c810c31ba5d9dee8237d1d22bb528a67
manu-neufeld <manuela.neufeld@gmail.com>
2021-01-11 11:02:33 +0000

    issues fixed

--- src/js/component/audioplayer.jsx
+++ src/js/component/audioplayer.jsx
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
 
 export function AudioComponent() {
-	// const [audioLibrary, setAudioLibrary] = useState([]);
 	const [song, setSong] = useState("files/mario/songs/castle.mp3");
 	const songURL = "https://assets.breatheco.de/apis/sound/";
-	const [fullLink, setFullLink] = useState("");
 	const [audioLibrary, setAudioLibrary] = useState([]);
 	let songsNames = [];
-	console.log(audioLibrary);
-	const [cancion, setcancion] = useState(songURL + song);
 
 	function myListgnerator() {
 		for (let index = 0; index < audioLibrary.length; index++) {
 			songsNames.push(
 				<li
+					className="item"
 					key={index}
 					onClick={() => {
 						setSong(audioLibrary[index].url);
@@ -27,47 +24,68 @@ export function AudioComponent() {
 		return songsNames;
 	}
 
+	const previousSong = () => {
+		for (let index = 0; index < audioLibrary.length; index++) {
+			if (audioLibrary[index].url == song) {
+				setSong(audioLibrary[index - 1].url);
+			}
+		}
+	};
+
+	const nextSong = () => {
+		for (let index = 0; index < audioLibrary.length; index++) {
+			if (audioLibrary[index].url == song) {
+				setSong(audioLibrary[index + 1].url);
+			}
+		}
+	};
+
 	useEffect(() => {
 		fetch("https://assets.breatheco.de/apis/sound/all")
 			.then(response => {
-				console.log("patata");
-				return response.json(); //devuelve un objeto
+				return response.json();
 			})
 			.then(responseAsJson => {
-				console.log(responseAsJson);
-				setAudioLibrary(responseAsJson["data/songs.json"]); //entre corchetes la key del object como valor un array con las canciones!!
+				setAudioLibrary(responseAsJson["data/songs.json"]);
 			});
-	}, []);
+	});
 	let myAudio = document.querySelector("#myAudioTag");
 
 	if (audioLibrary.length == 0) {
 		return "loading";
 	} else {
 		return (
-			<div>
+			<div className="audio-list">
 				<ul>{myListgnerator()}</ul>
 				<audio id="myAudioTag">
 					<source src={songURL + song} />
-					{(console.log(songURL + song), "ESTO ES SONGURL + SONG")}
 				</audio>
-				<button onClick={() => myAudio.play()}>Play</button>
-				{console.log(cancion, "ESTO ES EL PLAY")}
-				<button onClick={() => myAudio.pause()}>Pause</button>
-				<button onClick={() => (cancion.currentTime = 0)}>
-					Rewind
-				</button>
-				<button
-					onClick={() => {
-						setSong(audioLibrary[0 + 1].url);
-						console.log(
-							audioLibrary[0 + 1].url,
-							"ESTO ES NEXT SONG"
-						);
-						myAudio.load();
-						myAudio.play();
-					}}>
-					Next
-				</button>
+				<div className="controlBtn">
+					<button className="playBtn" onClick={() => myAudio.play()}>
+						Play
+					</button>
+					<button
+						className="pauseBtn"
+						onClick={() => myAudio.pause()}>
+						Pause
+					</button>
+					<button
+						onClick={() => {
+							previousSong();
+							myAudio.load();
+							myAudio.play();
+						}}>
+						<i className="fas fa-backward" />
+					</button>
+					<button
+						onClick={() => {
+							nextSong();
+							myAudio.load();
+							myAudio.play();
+						}}>
+						<i className="fas fa-forward" />
+					</button>
+				</div>
 			</div>
 		);
 	}
